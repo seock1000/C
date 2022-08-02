1061518534cc13b935bebce71464a1d937d41c27
seock1000 <tjrcjs0705@gmail.com>
2022-08-02 12:27:04 +0900

    Edit Doubly Linked List

---
+++ Data_Structure/DoublyLinkedList/DoublyLinkedList/DBLinkedListMain.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "DBLinkedList.h"
+
+int main() {
+	// 리스트 생성 및 초기화
+	List list;
+	int data;
+	ListInit(&list);
+
+	// 데이터 저장
+	LInsert(&list, 1); LInsert(&list, 2);
+	LInsert(&list, 3); LInsert(&list, 4);
+	LInsert(&list, 5); LInsert(&list, 6);
+	LInsert(&list, 7); LInsert(&list, 8);
+
+	// 데이터 조회
+	if (LFirst(&list, &data)) {
+		printf("%d ", data);
+
+		while(LNext(&list, &data))
+			printf("%d ", data);
+
+		while(LPrevious(&list, &data))
+			printf("%d ", data);
+
+		printf("\n\n");
+	}
+
+	return 0;
+}
