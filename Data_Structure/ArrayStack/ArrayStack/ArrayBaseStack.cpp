db3087c130469f2bdc51ae773405d61a955b2d41
seock1000 <tjrcjs0705@gmail.com>
2022-08-04 18:09:48 +0900

    Edit Array Base Stack

---
+++ Data_Structure/ArrayStack/ArrayStack/ArrayBaseStack.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayBaseStack.h"
+
+void StackInit(Stack* pstack) {
+	pstack->topIndex = -1;
+}
+
+int SIsEmpty(Stack* pstack) {
+	if (pstack->topIndex == -1)
+		return TRUE;
+	else
+		return FALSE;
+}
+
+void SPush(Stack* pstack, Data data) {
+	(pstack->topIndex)++;
+	pstack->stackArr[pstack->topIndex] = data;
+}
+
+Data SPop(Stack* pstack) {
+	int rIdx;
+
+	if (SIsEmpty(pstack)) {
+		printf("Stack Memory Error!");
+		exit(-1);
+	}
+
+	rIdx = pstack->topIndex;
+	(pstack->topIndex)--;
+
+	return pstack->stackArr[rIdx];
+}
+
+Data SPeek(Stack* pstack) {
+	if (SIsEmpty(pstack)) {
+		printf("Stack Memory Error!");
+		exit(-1);
+	}
+
+	return pstack->stackArr[pstack->topIndex];
+}
