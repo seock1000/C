18755cd8913efdc340ecbc2fd82489667620a7b7
seock1000 <tjrcjs0705@gmail.com>
2022-08-28 14:20:41 +0900

    Edit Quick Sort

---
+++ Data_Structure/QuickSort/QuickSort/QuickSort.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+// Swap data of array
+// input : array and two index to swap
+// ouput : void
+void Swap(int arr[], int idx1, int idx2) {
+	int temp = arr[idx1];
+	arr[idx1] = arr[idx2];
+	arr[idx2] = temp;
+}
+
+// seperate array to two area
+// input : array and first and last index of area to sort
+// output : index of pivot
+int Partition(int arr[], int left, int right) {
+	int pivot = arr[left];
+	int low = left + 1;
+	int high = right; // pivot과 low, high 설정
+
+	while (low <= high) { // low와 high가 교차될 때까지
+		while (pivot >= arr[low] && low <= right) // low 이동
+			low++;
+
+		while (pivot <= arr[high] && high >= (left + 1)) // high 이동
+			high--;
+
+		if (low <= high) // low와 high가 교차되지 않았으면 swap
+			Swap(arr, low, high);
+	}
+
+	Swap(arr, left, high); // high와 pivot swap
+	return high; // swap 이후 pivot의 위치 반환
+}
+
+// Quick sort
+// input : array and first and last index of array to sort
+// output : void
+void QuickSort(int arr[], int left, int right) {
+
+	if (left <= right) { // 재귀의 탈출 조건 : left > right 
+		int pivot = Partition(arr, left, right); // 분할하여 pivot의 위치 확정 후
+		QuickSort(arr, 0, pivot - 1);  
+		QuickSort(arr, pivot + 1, right); // 분할된 두 영역에 대해 정렬
+	}
+}
+
+int main() {
+	int arr[7] = { 3, 2, 4, 1, 7, 6, 5 };
+
+	int len = sizeof(arr) / sizeof(int);
+	int i;
+
+	QuickSort(arr, 0, sizeof(arr) / sizeof(int)-1);
+
+	for (i = 0; i < len; i++)
+		printf("%d ", arr[i]);
+
+	printf("\n");
+	return 0;
+}
