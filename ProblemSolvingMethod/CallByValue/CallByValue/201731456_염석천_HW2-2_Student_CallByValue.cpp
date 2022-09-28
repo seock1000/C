c56eb2f517e3e787e7229bab414e7eb41b6e2db5
seock1000 <tjrcjs0705@gmail.com>
2022-09-28 19:51:57 +0900

    Edit Call by Value as Homework

--- ProblemSolvingMethod/CallByValue/CallByValue/201731456_염석천_HW2-2_Student_CallByValue.cpp
+++ ProblemSolvingMethod/CallByValue/CallByValue/201731456_염석천_HW2-2_Student_CallByValue.cpp
@@ -1,23 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
-typedef struct _student {
+#define NAME_LEN 64
+#define NUM 2
+
+struct student {
 	char name[20];
 	int height;
 	float weight;
 	int schols;
-} Student;
+};
 
-void PrintInfo(Student stu) {
-
+// call by value
+struct student func(struct student stu) {
+	if (stu.height < 180) stu.height = 180;
+	if (stu.weight < 100) stu.weight = 100;
+	return stu;
 }
 
 int main() {
-	Student stu[2];
-	int i;
-
-	for (i = 0; i < 2; i++) {
-		printf("Type %d-th student's info (Name, height, weight, schols) : ");
-		scanf("%s %d %f %d", stu[i].name, stu[i].height, stu[i].weight, stu[i].schols);
+	struct student stu[NUM];
+	for (int i = 0; i < NUM; i++) {
+		printf("Type %d-th student's into (Name, height, weight, schols) : ", i);
+		scanf("%s %d %f %d", &stu[i].name, &stu[i].height, &stu[i].weight, &stu[i].schols);
 	}
+
+	for (int i = 0; i < NUM; i++) {
+		struct student tmp = func(stu[i]);
+		stu[i] = tmp;
+	}
+
+	for (int i = 0; i < NUM; i++) {
+		printf("Name    = %10s\n", stu[i].name);
+		printf("Height  = %10d\n", stu[i].height);
+		printf("Weight  = %10f\n", stu[i].weight);
+		printf("Schols  = %10d\n\n", stu[i].schols);
+	}
+
+	return 0;
 }
