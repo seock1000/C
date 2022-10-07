6bde16df9281c2e5fc60620ce19894bdb58eb653
seock1000 <tjrcjs0705@gmail.com>
2022-10-07 17:16:35 +0900

    Edit Fanshionable dressmaker's problem

--- Algorithm/FashionableDressmaker'sProblem/FashionableDressmaker'sProblem/FashionableDressmaker'sProblem.cpp
+++ Algorithm/FashionableDressmaker'sProblem/FashionableDressmaker'sProblem/FashionableDressmaker'sProblem.cpp
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNING
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,7 +8,36 @@ void InputData(FILE* fp, int* time, int* fine, int n) {
 	}
 }
 
-void main() {
+int MinIdx(int* arr, int n) {
+	int minIdx = 0;
+	for (int i = 0; i < n; i++) {
+		if (arr[minIdx] > arr[i])
+			minIdx = i;
+	}
+
+	return minIdx;
+}
+
+void FindSequence(int* time, int* fine, int n, int* seq) {
+	int* totalFine = (int*)malloc(sizeof(int) * n);
+	int i, j;
+
+	for (i = 0; i < n; i++)
+		seq[i] = i;
+
+	int tmp;
+	for (i = 1; i < n; i++) {
+		for (j = 0; j < n - i; j++) {
+			if (time[seq[j]] * fine[seq[j + 1]] > time[seq[j + 1]] * fine[seq[j]]) {
+				tmp = seq[j];
+				seq[j] = seq[j + 1];
+				seq[j + 1] = tmp;
+			}
+		}
+	}
+}
+
+int main() {
 	FILE* fp;
 	if ((fp = fopen("test.txt", "r")) == NULL) {
 		printf("File open failed!\n");
@@ -28,7 +57,19 @@ void main() {
 		fscanf(fp, "%d", &task);
 		int* time = (int*)malloc(sizeof(int) * task);
 		int* fine = (int*)malloc(sizeof(int) * task);
+		int* seq = (int*)malloc(sizeof(int) * task);
 
-		
+		InputData(fp, time, fine, task);
+		FindSequence(time, fine, task, seq);
+
+		for (int i = 0; i < task; i++) {
+			printf("%d ", seq[i] + 1);
+		}
+		printf("\n");
+
+		free(time);
+		free(fine);
+		free(seq);
 	}
+	return 0;
 }
